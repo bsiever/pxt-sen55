a7819ec98969da3d2118721a4404691401a1834b
Bill Siever <bsiever@gmail.com>
2023-05-31 17:06:37 -0500

    Updates.

--- sen55.ts
+++ sen55.ts
@@ -6,12 +6,40 @@
 //% groups="['Advanced']"
 namespace Sen55 {
 
-// //% blockId=onButtonSingleClicked block="on button |%NAME single clicked"
-// //% weight=100 
-// export function onButtonSingleClicked(button: AorB, body: Action) {
-//     let buttonHandlers = actions.get(button)
-//     buttonHandlers.set(SINGLECLICK, body)
-// }
+    const i2cAddr = 0x69;
 
+/** 
+ * Return null if the buffer doesn't pass the checksum; Otherwise
+ * buffer with raw data (checksums removed)
+ */
+
+
+
+    function checkBuffer(b: Buffer) : Buffer {
+        return null;
+    }
+
+
+
+    //% block="produce name"
+    export function productName(): string {
+      // Format:  Command, Delay, buffer returned
+      pins.i2cWriteNumber(i2cAddr, 0xD014, NumberFormat.UInt16BE, false)
+      pause(20)
+      let buffer = pins.i2cReadBuffer(i2cAddr, 32, false)
+      let name = ''
+      for(let i=0;i<buffer.length;i++) {
+        let char = buffer.getUint8(i)
+        if(char == 0) {
+            serial.writeLine("Got to null at" + i)
+            return name;
+        }
+        let stringForChar = String.fromCharCode(char&0x7f)
+        serial.writeLine("Code: " + stringForChar + "(" + char+")")
+        name = name.concat(stringForChar)
+        serial.writeLine(name)
+      }
+      return "NONE"
+    }
 
 }
--- test.ts
+++ test.ts
@@ -1,2 +1,7 @@
 serial.writeLine("Starting...")
 
+input.onButtonPressed(Button.A, function () {
+    serial.writeLine("Name = ")
+    serial.writeLine(Sen55.productName() + "!")
+})
+basic.showNumber(6)
